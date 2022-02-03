0b84ece5ee6bb2ad8ff380a1a73e685c5f13e4bd
Ramzan <ramzan.ali.suzan@gmail.com>
2022-02-03 22:47:08 +0600

    array implementation done

---
+++ src/data-structures/arrays/CSharp/Arrays/Arrays/Array.cs
@@ -0,0 +1,118 @@
+﻿namespace Arrays
+{
+    internal class Array
+    {
+        private int[] items;
+        private int count;
+
+        public Array(int size)
+        {
+            items = new int[size];
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+
+        public void Insert(int element)
+        {
+            if (count == items.Length)
+            {
+                var newItems = new int[count * 2];
+
+                for (int i = 0; i < count; i++)
+                    newItems[i] = items[i];
+
+                items = newItems;
+            }
+
+            items[count++] = element;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            for (int i = index; i < count; i++)
+                items[i] = items[i + 1];
+
+            count--;
+        }
+
+        public int IndexOf(int element)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] == element)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public int Max()
+        {
+            var max = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] > max)
+                    max = items[i];
+            }
+
+            return max;
+        }
+
+        public int[] Intersect(int[] second)
+        {
+            var x = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if (items[i] == second[j])
+                    {
+                        x[j] = second[j];
+                    }
+                }
+            }
+
+            return x;
+        }
+
+        public int[] Reverse()
+        {
+            var reverse = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                reverse[i] = items[(count - 1) - i];
+            }
+
+            return reverse;
+        }
+
+        public void InsertAt(int element, int index)
+        {
+            if (index < 0 || index >= count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            var newArray = new int[count + 1];
+
+            for (int i = index; i < count; i++)
+            {
+                newArray[i + 1] = items[i];
+            }
+
+            items[index] = element;
+
+            items = newArray;
+
+            count++;
+        }
+    }
+}
---
+++ src/data-structures/arrays/CSharp/Arrays/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new Array(2);
+            array.Insert(20);
+            array.Insert(30);
+            array.Insert(40);
+
+            array.InsertAt(10, 0);
+
+            array.Print();
+
+            Console.WriteLine();
+        }
+    }
+}
